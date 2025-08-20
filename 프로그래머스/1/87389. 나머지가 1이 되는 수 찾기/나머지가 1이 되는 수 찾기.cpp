0e0f3e59f2d124a7006eed8650cbd4ac77c6908a
khee8469 <61371373+khee8469@users.noreply.github.com>
2025-08-20 09:56:43 +0900

    [level 1] Title: 나머지가 1이 되는 수 찾기, Time: 2.26 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/87389. 나머지가 1이 되는 수 찾기/나머지가 1이 되는 수 찾기.cpp
@@ -0,0 +1,17 @@
+int solution(int n) 
+{
+    if (n % 2 == 1)
+    {
+        return 2;
+    }
+    else
+    {
+        for (int i = 3; i < n; ++i)
+        {
+            if (n % i == 1)
+            {
+                return i;
+            }
+        }
+    }
+}

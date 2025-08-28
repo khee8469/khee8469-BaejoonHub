6e5ade7d13a742ec94a1eac140d945492ce91d33
khee8469 <61371373+khee8469@users.noreply.github.com>
2025-08-28 09:24:33 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.cpp
@@ -0,0 +1,20 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> numbers)
+{
+    int answer = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        auto a = find(numbers.begin(), numbers.end(), i);
+        if (a == numbers.end())
+        {
+            answer += i;
+        }
+    }
+
+    return answer;
+}
+

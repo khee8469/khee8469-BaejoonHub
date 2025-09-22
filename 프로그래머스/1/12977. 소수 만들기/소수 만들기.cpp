559565560969325a2f75afff827cc0b4a24c2508
khee8469 <61371373+khee8469@users.noreply.github.com>
2025-09-22 09:45:55 +0900

    [level 1] Title: 소수 만들기, Time: 4.58 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,36 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(vector<int> nums) 
+{
+    int answer = 0;
+    
+    for(int i =0; i<nums.size()-2; i++)
+    {
+        for(int j = i+1; j<nums.size()-1; j++)
+        {  
+            for(int k = j+1; k<nums.size(); k++)
+            {
+                int sum = nums[i]+nums[j]+nums[k];
+                bool cheak = false;
+                
+                for(int l = 2; l < sum/2; l++)
+                {
+                    if(sum % l == 0)
+                    {
+                        cheak = true;
+                        break;   
+                    }
+                }
+                if(!cheak)
+                {
+                    answer++;
+                }
+            }
+        }
+    }
+    
+
+    return answer;
+}
